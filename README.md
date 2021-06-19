150d947643065196030270b2f37f4df8a32c1a76
FernandoJSM <53662620+FernandoJSM@users.noreply.github.com>
2021-06-19 09:07:11 -0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# CorreiosTracker
+# Rastreador de Pacotes
+
+Prova conceitual de aquisição de dados de rastreio a partir do site dos Correios BR.
+
+Uma requisição ao site oficial é feita com o código de rastreio e o parsing do site retornado é feito com a biblioteca
+BeautifulSoup para imprimir as informações do código no console.
+
+### Falta validar:
+* Inserção de vários códigos;
+* Identificar quando o código de rastreio não retorna resultados.
+
+### Possíveis implementações:
+#### Bot de Telegram:
+Um bot onde os códigos são cadastrados e a cada atualização o usuário é notificado via mensagem.
+
+#### Análise dos códigos próximos:
+Os códigos de rastreio são gerados em uma certa ordem, dessa forma, a partir de um código é possível gerar outros códigos
+que em teoria foram criados próximo do original. Assim, no caso de importações, é possível visualizar se estes códigos
+próximos já chegaram no Brasil e se já foram liberados da fiscalização. No fim das contas não é nada preciso, serve só
+como curiosidade.
---
+++ main.py
@@ -0,0 +1,39 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+
+url = "https://www2.correios.com.br/sistemas/rastreamento/ctrl/ctrlRastreamento.cfm?"
+data = {
+    "acao": "track",
+    "objetos": "AA000000000AA"  # Código de rastreio aqui no formato AA000000000AA
+}
+
+page = requests.post(url=url, data=data)
+soup = BeautifulSoup(markup=page.content, features='html.parser')
+
+events = soup.find_all(name="table", class_="listEvent sro")
+
+for event in events:
+    date_raw = event.find_all(name="td", class_="sroDtEvent")[0].text
+    date = re.search(r"[0-9]{2}/[0-9]{2}/[0-9]{4}", date_raw)[0]
+
+    time = re.search(r"[0-9]{2}:[0-9]{2}", date_raw)[0]
+
+    location_raw = date_raw.replace('\n', '').replace('\r', '').replace('\t', '')
+    location = re.search(r"(?<=:[0-9]{2}).*", location_raw)[0].strip()
+
+    event_raw = event.find_all(name="td", class_="sroLbEvent")[0].text
+    event_raw = re.sub('\t+', ' ', event_raw)
+    event_raw = re.sub('\n+', ' ', event_raw)
+    event_raw = re.sub('\r+', ' ', event_raw)
+    event_raw = re.sub('\s+', ' ', event_raw)
+    event_str = event_raw.strip()
+
+    info_index = event_str.find('Informar')
+    if info_index != -1:
+        point_index = info_index - 1
+        event_str = event_str[:point_index] + '.' + event_str[point_index:]
+
+    print(f'{date} {time} - {location}:')
+    print(event_str)
+    print('')
